33468c55f5826b7e9a732769dc6a29829d796b9f
bhagyashri-cj <70849125+bhagyashri-cj@users.noreply.github.com>
2022-09-26 15:57:49 +0530

    Update App.java

--- src/main/java/newlearnwell/app/App.java
+++ src/main/java/newlearnwell/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Devops Jenkins!" );
+        System.out.println( "Hurrye Jenkins is Working!!!!!!!" );
     }
 }
